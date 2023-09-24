21ce6fb0a220a1bf0c59dca23dc1210464de12f4
Felix Irwanto <felixirwanto25@gmail.com>
2023-09-24 18:37:29 +0800

    add js cors

--- akar_kuadrat.js
+++ akar_kuadrat.js
@@ -1,13 +1,17 @@
 // Import necessary libraries
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors'); // Import the CORS library
 
 const app = express();
 const port = 3306;
 
+// Enable CORS for all routes
+app.use(cors());
+
 // Database configuration
 const dbConfig = {
-  host: 'https://sql12.freesqldatabase.com',
+  host: 'sql12.freesqldatabase.com',
   user: 'sql12647777',
   password: 'EFPDG6CIrK',
   database: 'sql12647777',
@@ -16,9 +20,9 @@ const dbConfig = {
 // Create a database connection pool
 const pool = mysql.createPool(dbConfig);
 
-// API endpoint to receive a numbers from the URL, calculate the square root, and insert into the database
+// API endpoint to receive numbers from the URL, calculate the square root, and insert into the database
 app.get('/api/calculateSquareRoot', (req, res) => {
-  const numbers = parseFloat(req.query.numbers); // Extract the numbers from the URL query parameter
+  const numbers = parseFloat(req.query.numbers);
 
   if (isNaN(numbers)) {
     return res.status(400).json({ error: 'Invalid number' });
